a59317d41e5c1e8355a50f04e060ec02f811308b
Rajsa94 <115164768+Rajsa94@users.noreply.github.com>
2022-11-23 07:37:23 +0530

    first commit

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require("path")
 const app = express();
 const hbs = require('hbs')
-const port = 8000
+const port = process.env.PORT || 3300
 
 // public static pth
 const static_path = path.join(__dirname, "../public");
